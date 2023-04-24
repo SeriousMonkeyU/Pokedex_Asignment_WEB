7c122b6a99c59d4c59f4f5b96cf60fbf66ee319e
SeriousMonkeyU <berabbitshy@gmail.com>
2023-04-24 03:28:20 +0200

    .

--- src/index.js
+++ src/index.js
@@ -7,9 +7,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
     <App />
-    <Routes>
-      <Route path="/" />
-      <Route path="/about" />
-    </Routes>
   </HashRouter>
 );
